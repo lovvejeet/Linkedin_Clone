40056ad115827b4ba4abd68f20fccaa1074a7ff5
lovvejeet <lovejeetsandhu8194@gmail.com>
2024-10-01 17:36:19 +0530

    auth setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import authRoutes from './routes/auth.routes.js'
 
 const app = express();
 const PORT=5000;
+
+app.use('/api/v1/auth',authRoutes)
 
 app.listen(PORT,()=>{
 console.log(`Server running at ${PORT}`)
