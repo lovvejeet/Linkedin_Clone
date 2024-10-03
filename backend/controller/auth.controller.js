07658dcd8b04dfcc26a9fbd69c0cc33d8171b6f4
lovvejeet <lovejeetsandhu8194@gmail.com>
2024-10-03 13:05:38 +0530

    added sign up function

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,5 +1,60 @@
-export const signup = (req, res) => {
-  res.send("signup");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+  try {
+    const { name, username, email, password } = req.body;
+    if (!name || !username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const existingEmail = await User.findOne({ email });
+
+    if (existingEmail) {
+      return res
+        .status(400)
+        .json({ message: "An account with this email already exists." });
+    }
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res
+        .status(400)
+        .json({ message: "An account with this username already exists." });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long." });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      username,
+    });
+    await user.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    res.cookie("jwt-linkedin", token, {
+      httpOnly: true,
+      maxAge: 3 * 24 * 60 * 60 * 1000,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({ message: "User registered successfully" });
+
+    //  to send mail
+  } catch (error) {
+    console.log("Error in signup: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 export const login = (req, res) => {
   res.send("login");
---
+++ backend/test.mailtrap.js
@@ -0,0 +1,27 @@
+import { MailtrapClient } from "mailtrap";
+
+const TOKEN = process.env.MAILTRAP_TOKEN;
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "lovvejeetsandhu@gmail.com",
+  },
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
